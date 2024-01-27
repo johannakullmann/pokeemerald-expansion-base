b42c176b1fb5148d2f5ef83dd760cce5f48af359
johannakullmann <151456919+johannakullmann@users.noreply.github.com>
2024-01-27 18:20:06 +0100

    slateport things

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -413,7 +413,6 @@
 #define MAP_ALTERING_CAVE                            (106 | (25 << 8))
 #define MAP_METEOR_FALLS_STEVENS_CAVE                (107 | (25 << 8))
 #define MAP_ROUTE136_GROVE                           (108 | (25 << 8))
-#define MAP_MOUNTAIN_CAVE                            (109 | (25 << 8))
 
 // gMapGroup_IndoorDynamic
 #define MAP_SECRET_BASE_RED_CAVE1    (0 | (26 << 8))
