de699cf57140f1c60183082b387bc8285fc27f3e
johannakullmann <151456919+johannakullmann@users.noreply.github.com>
2024-01-30 19:08:43 +0100

    Roar nerf + roar wore off message

--- src/fldeff_roar.c
+++ src/fldeff_roar.c
@@ -2,15 +2,14 @@
 #include "event_data.h"
 #include "event_scripts.h"
 #include "field_effect.h"
-#include "field_player_avatar.h"
 #include "fldeff.h"
 #include "party_menu.h"
 #include "script.h"
-#include "sprite.h"
+#include "string_util.h"
 #include "task.h"
-#include "wild_encounter.h"
 #include "constants/field_effects.h"
-#include "constants/songs.h"
+
+#define NUM_ROAR_REPEL_STEPS 20
 
 static void FieldCallback_Roar(void);
 void StartRoarFieldEffect(void);
@@ -26,6 +25,11 @@ static void FieldCallback_Roar(void)
 {
     FieldEffectStart(FLDEFF_ROAR);
     gFieldEffectArguments[0] = GetCursorSelectionMonId();
+    VarSet(VAR_REPEL_STEP_COUNT, NUM_ROAR_REPEL_STEPS);
+    #if VAR_LAST_REPEL_LURE_USED != 0
+        VarSet(VAR_LAST_REPEL_LURE_USED, ITEM_NONE);
+    #endif
+    ScriptContext_SetupScript(EventScript_UseRoar);
 }
 
 bool8 FldEff_Roar(void)
@@ -34,15 +38,12 @@ bool8 FldEff_Roar(void)
     taskId = CreateFieldMoveTask();
     gTasks[taskId].data[8] = (u32)StartRoarFieldEffect >> 16;
     gTasks[taskId].data[9] = (u32)StartRoarFieldEffect;
+    GetMonNickname(&gPlayerParty[gFieldEffectArguments[0]], gStringVar1);
     return FALSE;
 }
 
 void StartRoarFieldEffect(void)
 {
     FieldEffectActiveListRemove(FLDEFF_ROAR);
-    VarSet(VAR_REPEL_STEP_COUNT, 50);
-    #if VAR_LAST_REPEL_LURE_USED != 0
-        VarSet(VAR_LAST_REPEL_LURE_USED, ITEM_REPEL_ROAR);
-    #endif
-    ScriptContext_SetupScript(EventScript_UseRoar);
+    ScriptContext_Enable();
 }
